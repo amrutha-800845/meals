95c2f72d501ad5be9970be23052ebd5c7eb20d5f
amrutha-800845 <yempalakuamrutha@gmail.com>
2026-09-24 09:57:57 +0530

    home page creation

---
+++ script.js
@@ -0,0 +1,55 @@
+let meals=document.getElementById("meals");
+async function user() {
+    let resolve=await fetch("https://www.themealdb.com/api/json/v1/1/categories.php");
+    let data=await resolve.json();
+    let result=data.categories.map((value)=>{
+         return `<div class="main" >
+         <button class="value"><h6>${value.strCategory}</h6><hr>
+         </div>`
+        
+    })
+    meals.innerHTML+=result.join("")
+    
+}
+user();
+
+/*   categories pics */
+let card=document.getElementById("card");
+async function pic() {
+    let response=await fetch("https://www.themealdb.com/api/json/v1/1/categories.php")
+    let data=await response.json();
+    let result=data.categories.map((value)=>{
+        return ` <div class="pic1">
+        <h6 class="hi">${value.strCategory}</h6>
+        <img src="${value.strCategoryThumb}">
+
+
+        </div> `
+    })
+   card.innerHTML += result.join("");
+    }
+pic();
+/* filter */
+    
+
+async function name() {
+    let fill=document.getElementById("fill");
+    let search=document.getElementById("search").value.toLowerCase().trim();
+    if(search===""){
+        fill.innerHTML=""
+    }
+    let rej=await fetch(`http://www.themealdb.com/api/json/v1/1/search.php?s=${search}`);
+    let ram=await rej.json();
+    let data=ram.meals.map((item)=>{
+        return `<div>
+        <h6>${item.strCategory}</h6>
+        <img src="${item.strMealThumb}">
+        <p>${item.strArea}</p>
+        <h5>${item.strMeal}</h5>
+        </div>`
+    })
+    fill.innerHTML=data.join("")
+        
+    
+}
+ 
